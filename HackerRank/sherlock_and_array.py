f78bb6879625e4390de0c6f6e799c6cf7b063d42
Priya Patel <118622157+Coder-priya@users.noreply.github.com>
2023-11-16 00:01:39 +0530

    Create sherlock_and_array.py

---
+++ HackerRank/sherlock_and_array.py
@@ -0,0 +1,15 @@
+def balancedSums(arr):
+    # Write your code here
+    is_balanced = 'NO'
+    
+    total_sum = sum(arr)
+    left_sum = 0
+    
+    for num in arr:
+        if left_sum != total_sum - left_sum - num:
+            left_sum += num
+        else:
+            is_balanced = 'YES'
+            break
+    
+    return is_balanced
